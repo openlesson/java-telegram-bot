204285e665ebac8a1bf88da68fda59e042b135db
Maxim <maxshaptala@gmail.com>
2020-12-03 16:28:19 +0200

    Removed EchoBot.java and added /start command to WeatherBot

--- src/main/java/org/itstep/bot/WeatherBot.java
+++ src/main/java/org/itstep/bot/WeatherBot.java
@@ -1,5 +1,6 @@
 package org.itstep.bot;
 
+import org.itstep.Config;
 import org.itstep.weather.OpenWeatherMap;
 import org.itstep.weather.data.Coord;
 import org.itstep.weather.data.Main;
@@ -8,28 +9,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.telegram.abilitybots.api.bot.AbilityBot;
 import org.telegram.abilitybots.api.objects.Ability;
-import org.telegram.abilitybots.api.objects.Privacy;
-import org.telegram.abilitybots.api.objects.Reply;
-import org.telegram.abilitybots.api.objects.ReplyFlow;
+import org.telegram.abilitybots.api.util.AbilityUtils;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Location;
-import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardRemove;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
-import java.util.Map;
 import java.util.Optional;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
 
-import static org.telegram.abilitybots.api.objects.Locality.*;
-import static org.telegram.abilitybots.api.objects.Privacy.ADMIN;
+import static org.telegram.abilitybots.api.objects.Locality.ALL;
 import static org.telegram.abilitybots.api.objects.Privacy.PUBLIC;
-import static org.telegram.abilitybots.api.util.AbilityUtils.getChatId;
 
 public class WeatherBot extends AbilityBot {
 
@@ -39,57 +31,19 @@ public class WeatherBot extends AbilityBot {
 
     public WeatherBot(String botToken, String botUsername) {
         super(botToken, botUsername);
-        openWeatherMap = new OpenWeatherMap(System.getenv("OPEN_WEATHER_MAP_API"));
-    }
-
-//    public Reply sayYuckOnImage() {
-//        // getChatId is a public utility function in rg.telegram.abilitybots.api.util.AbilityUtils
-//        Consumer<Update> action = upd -> silent.send("You said hi", getChatId(upd));
-//
-//        Reply replyHi = Reply.of(action, u -> u.hasMessage() && u.getMessage().getText().equalsIgnoreCase("hi"));
-//        Reply replyHello = Reply.of(update -> silent.send("You said hello", getChatId(update)),
-//                update -> update.hasMessage() && update.getMessage().getText().equalsIgnoreCase("hello"));
-//        return ReplyFlow.builder(db)
-//                .next(replyHi)
-//                .next(replyHello)
-//                .build();
-//    }
-
-    public Ability usersCommand() {
-        return Ability
-                .builder()
-                .name("users")
-                .privacy(Privacy.CREATOR)
-                .locality(ALL)
-                .input(0)
-                .action(messageContext -> {
-                    String msg = users().values().stream().map(User::getFirstName).collect(Collectors.joining(", "));
-                    silent.send(msg, getChatId(messageContext.update()));
-                })
-                .build();
-    }
-
-    public Ability sayHelloWorld() {
-        return Ability
-                .builder()
-                .name("hello")
-                .info("says hello world!")
-                .input(0)
-                .locality(USER)
-                .privacy(ADMIN)
-                .action(ctx -> silent.send("Hello world!", ctx.chatId()))
-                .post(ctx -> silent.send("Bye world!", ctx.chatId()))
-                .build();
+        openWeatherMap = new OpenWeatherMap(Config.getOpenWeatherMapApiKey());
     }
 
     public Ability start() {
-        return Ability.builder()
+        return Ability
+                .builder()
                 .name("start")
-                .info("Simple weather bot")
+                .info("Start your bot")
                 .input(0)
                 .locality(ALL)
                 .privacy(PUBLIC)
-                .action(ctx -> silent.send("Hello " + ctx.user().getUserName(), ctx.chatId()))
+                .action(messageContext -> silent.send("Type /weather command to get current weather for your location",
+                        AbilityUtils.getChatId(messageContext.update())))
                 .build();
     }
 
@@ -110,7 +64,6 @@ public class WeatherBot extends AbilityBot {
                 .reply(update -> {
                     Location loc = update.getMessage().getLocation();
                     logger.info("Location: {}", loc);
-                    // TODO: вернуть погоду для указанной локации
                     SendMessage sendMessage = new SendMessage();
                     Optional<WeatherData> currentWeather = openWeatherMap.getCurrentWeather(new Coord(loc.getLongitude(), loc.getLatitude()));
                     if(currentWeather.isPresent()) {
