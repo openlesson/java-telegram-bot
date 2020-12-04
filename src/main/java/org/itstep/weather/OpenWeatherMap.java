4602c4fc56830112bba281a3fcac7c10e8b2f401
Maxim <maxshaptala@gmail.com>
2020-12-04 23:15:23 +0200

    Removed unnecessary import

--- src/main/java/org/itstep/weather/OpenWeatherMap.java
+++ src/main/java/org/itstep/weather/OpenWeatherMap.java
@@ -1,7 +1,6 @@
 package org.itstep.weather;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectReader;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
